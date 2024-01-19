214af8cb465db89b0ca3d19158e96674489ff892
serch <sergioibanezmoreno@gmail.com>
2024-01-19 14:47:40 +0100

    Lo de isSSR no vale para nada aun, fuera

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,10 @@ const routes = {
     home: {
         path: '/',
         pageComponent: Home,
-        isSSR: true
     },
     profile: {
         path: '/profile',
         pageComponent: Profile,
-        isSSR: false
     }
 }
 
