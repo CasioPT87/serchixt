3faf7e9e68be9554eaea35b2a10e2207ebba410b
serch <sergioibanezmoreno@gmail.com>
2024-02-26 16:44:21 +0100

    Deleted system tools

--- system-tools/stop-process.js
+++
@@ -1,9 +0,0 @@
-console.log('HOLAAAAA', process)
-
-process.on('exit', (code) => {
-    console.log(`About to exit with code ${code}`);
-    // Perform cleanup tasks here
-  });
-
-process.kill(process.pid, 'SIGTERM');
-process.exit(1)
