64c53087d86da1608f8ae076ab785eab84e76ce5
serch <sergioibanezmoreno@gmail.com>
2024-07-26 17:34:12 +0200

    transpiled folder removed

--- transpiled/src/hooks/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usePreloadData = void 0;
-const usePreloadData_js_1 = __importDefault(require("./usePreloadData.js"));
-exports.usePreloadData = usePreloadData_js_1.default;
-//# sourceMappingURL=index.js.map
