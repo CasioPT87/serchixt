d14d748f717c634a89b45c7f3320f1e84ef4fe62
serch <sergioibanezmoreno@gmail.com>
2024-08-01 09:50:37 +0200

    Added pieces for auth in server

--- app.ts
+++ app.ts
@@ -3,21 +3,35 @@ require('@babel/register')({
   ignore: [/node_modules/], // Exclude node_modules from being transpiled
 });
 
-const express = require('express');
+import express, { Request, Response } from 'express';
+const cookieParser = require('cookie-parser')
 const path = require('path')
 const initial = require('./renderers/initial')
 const { getAllRoutes, getAllowedPage } = require('./tools')
+const { authenticate } = require('./server-utils/')
 
 const PORT = 9990;
 const app = express()
 
-const userIsLogged = false
-
 // Serve static files
 app.use(express.static(path.join(__dirname, '../../src/dist')));
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// Middleware to catch errors in async middleware/handlers
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+}
+
+app.use(asyncHandler(async (req, res, next) => {
+  await authenticate(req);
+  next();
+}));
+
 // All allowed routes
-app.get(getAllRoutes(), async (req: any, res: any) => {
+app.get(getAllRoutes(), async (req: Request, res: Response) => {
   const path = req.path
   const { page, isRedirection } = getAllowedPage({ path })
   if (isRedirection) {
@@ -25,6 +39,12 @@ app.get(getAllRoutes(), async (req: any, res: any) => {
   }
 
   await initial.default({ response: res, page })
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
 // Start the server
---
+++ server-utils/index.ts
@@ -0,0 +1,23 @@
+const secret = process.env.SECRET
+
+const cookieName = 'garriga-app'
+const backendUrl = process.env.BACKEND_URL || ''
+const backendAuthPath = process.env.BACKEND_AUTH_PATH || ''
+
+const authenticate = async (req: Request) => {
+  try {
+    const cookieToken = req.cookies[cookieName];
+    const jwtToken = req?.query?.token
+    const token = jwtToken || cookieToken // order matters, first we check the value of the query value, then the cookie
+    const authUrl = backendUrl + backendAuthPath
+    const response = await fetch(authUrl)
+    if (!response.ok) throw new Error('Authentication failed')
+    // this is just an example
+    const data = await response.json()
+    if (data.authenticated) return true
+    return false
+  } catch (e) {
+    console.error(e.message)
+    return false
+  }
+};
